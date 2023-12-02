85227f9380a927e43e53e827af96050339d354ca
Jorge0rtega <89051851+Jorge0rtega@users.noreply.github.com>
2023-12-02 03:48:29 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,15 @@
-// create web server
-// run server
-// open browser
-// localhost:3000/comments
-// localhost:3000/comments?postId=1
-// localhost:3000/comments?postId=1&id=1
-
-const http = require('http');
-const url = require('url');
-const comments = require('./data/comments.json');
-
-const server = http.createServer((req, res) => {
-  //console.log(req.url);
-  //console.log(url.parse(req.url, true));
-  const parsedUrl = url.parse(req.url, true);
-  //console.log(parsedUrl.query);
-  const { postId, id } = parsedUrl.query;
-  //console.log(postId, id);
-  //console.log(comments);
-  const filteredComments = comments.filter(
-    (comment) =>
-      comment.postId === (postId ? parseInt(postId) : comment.postId) &&
-      comment.id === (id ? parseInt(id) : comment.id)
-  );
-  //console.log(filteredComments);
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(filteredComments));
+// create web server 
+// 1. import express
+const express = require('express');
+// 2. create an instance of express
+const app = express();
+// 3. create a port number
+const port = 3000;
+// 4. create a route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
-
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// 5. start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
